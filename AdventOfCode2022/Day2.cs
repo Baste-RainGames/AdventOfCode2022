5c6aa7652ae002b173bb3dd5cff4fd31eb29c535
Baste Nesse Buanes <baste@rain-games.com>
2022-12-03 12:10:05 +0100

    lol this is too compact by far

--- AdventOfCode2022/Day2.cs
+++ AdventOfCode2022/Day2.cs
@@ -1,63 +1,26 @@
 namespace AdventOfCode2022;
-using static Day2.Hand;
-using static Day2.Result;
 
 public static class Day2 {
     public static void Run(bool example) {
         var data = File.ReadAllLines(example ? "day2Example" : "day2Input");
 
-        var score = 0;
+        var scorePuzzle1 = 0;
+        var scorePuzzle2 = 0;
         foreach (var line in data) {
-            // 0: rock, 1: paper, 2: scissors
-            var opponentHand = line[0] - 'A';
+            // puzzle 1:
+            var opponentHand = line[0] - 'A'; // 0: rock, 1: paper, 2: scissors
             var myHand       = line[2] - 'X';
-
-            score += myHand +  1; // own score
-            score += (((myHand - opponentHand) + 4) % 3) * 3; // result score
+            
+            scorePuzzle1 += myHand +  1; // own score
+            scorePuzzle1 += (((myHand - opponentHand) + 4) % 3) * 3; // result score
+            
+            // puzzle 2:
+            var desiredResult = myHand; // 0: loss, 1: draw, 2: win
+            var ownHand = (opponentHand + desiredResult + 2) % 3;
+            scorePuzzle2 += desiredResult * 3 + ownHand + 1;
         }
 
-        Console.Out.WriteLine($"Problem 1 is: {score}");
-
-        score = 0;
-        foreach (var line in data) {
-            var opponentHand = (Hand) (line[0] - 'A');
-            var desiredResult = line[2] switch {
-                'X' => Result.Loss,
-                'Y' => Result.Draw,
-                'Z' => Result.Win,
-            };
-
-            var ownHand = opponentHand switch {
-                Rock     when desiredResult is Loss => Scissors,
-                Rock     when desiredResult is Draw => Rock,
-                Rock     when desiredResult is Win  => Paper,
-                Paper    when desiredResult is Loss => Rock,
-                Paper    when desiredResult is Draw => Paper,
-                Paper    when desiredResult is Win  => Scissors,
-                Scissors when desiredResult is Loss => Paper,
-                Scissors when desiredResult is Draw => Scissors,
-                Scissors when desiredResult is Win  => Rock,
-            };
-
-            score += (int) desiredResult + (int) ownHand + 1;
-        }
-        
-        Console.Out.WriteLine($"Problem 2 is: {score}");
-    }
-
-    public static Hand GetHand(char c) {
-        return (Hand)(c - 'A');
-    }
-
-    public enum Hand {
-        Rock     = 0,
-        Paper    = 1,
-        Scissors = 2
-    }
-
-    public enum Result {
-        Loss = 0,
-        Draw = 3,
-        Win = 6
+        Console.Out.WriteLine($"Problem 1 is: {scorePuzzle1}");
+        Console.Out.WriteLine($"Problem 2 is: {scorePuzzle2}");
     }
 }
