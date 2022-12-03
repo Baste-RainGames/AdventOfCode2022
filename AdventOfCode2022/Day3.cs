d0005280c401736593ba9cb5ef093108d37dec12
Baste Nesse Buanes <baste@rain-games.com>
2022-12-03 12:46:31 +0100

    Day 3 ezpz

--- AdventOfCode2022/Day3.cs
+++ AdventOfCode2022/Day3.cs
@@ -1,8 +1,41 @@
+using Console = System.Console;
+
 namespace AdventOfCode2022;
 
 public static class Day3 {
     public static void Run(bool example) {
         var data = File.ReadAllLines(example ? "day3Example" : "day3Input");
-        Console.Out.WriteLine("Not implemented yet!");
+
+        var prioritySum = 0;
+        foreach (var rucksack in data) {
+            var compartmentSize = rucksack.Length / 2;
+            var firstCompartment = rucksack.Take(compartmentSize);
+            var secondCompartment = rucksack.Skip(compartmentSize).Take(compartmentSize);
+
+            var sharedElements = firstCompartment.Intersect(secondCompartment);
+            var singleSharedElement = sharedElements.Single();
+
+            var priority = GetPriority(singleSharedElement);
+            prioritySum += priority;
+        }
+
+        Console.Out.WriteLine("first puzzle: " + prioritySum);
+
+        var groupPrioritySum = 0;
+        for (int i = 0; i < data.Length; i+= 3) {
+            var rucksack0 = data[i + 0];
+            var rucksack1 = data[i + 1];
+            var rucksack2 = data[i + 2];
+
+            var sharedElement = rucksack0.Intersect(rucksack1).Intersect(rucksack2).Single();
+
+            groupPrioritySum += GetPriority(sharedElement);
+        }
+        
+        Console.Out.WriteLine("second puzzle: " + groupPrioritySum);
+    }
+
+    private static int GetPriority(char item) {
+        return item <= 'Z' ? item - 'A' + 27 : item - 'a' + 1;
     }
 }
