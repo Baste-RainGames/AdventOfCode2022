d5c66be50f83ac2d148c565d852badc3f44936a6
Baste Nesse Buanes <baste@rain-games.com>
2022-12-04 10:11:35 +0100

    Day 4 no biggie

--- AdventOfCode2022/Day4.cs
+++ AdventOfCode2022/Day4.cs
@@ -3,6 +3,24 @@ namespace AdventOfCode2022;
 public static class Day4 {
     public static void Run(bool example) {
         var data = File.ReadAllLines(example ? "day4Example" : "day4Input");
-        Console.Out.WriteLine("Not implemented yet!");
+
+        var sectionPairs = data.Select(s => s.Split(',', '-'))
+                               .Select(strs => Array.ConvertAll(strs, int.Parse))
+                               .ToArray();
+
+        var fullOverlaps = 0;
+        var partialOverlaps = 0;
+        foreach (var sectionPair in sectionPairs) {
+            var (from0, to0, from1, to1) = (sectionPair[0], sectionPair[1], sectionPair[2], sectionPair[3]);
+
+            if ((from0 >= from1 && to0 <= to1) || (from0 <= from1 && to0 >= to1))
+                fullOverlaps++;
+
+            if (!(to0 < from1 || to1 < from0))
+                partialOverlaps++;
+        }
+
+        Console.Out.WriteLine("puzzle 1 (full overlaps):" + fullOverlaps);
+        Console.Out.WriteLine("puzzle 2 (partial overlaps):" + partialOverlaps);
     }
 }
