7013c09ebeef6c10959fc2969b522803fbd72df2
Baste Nesse Buanes <baste@rain-games.com>
2022-12-15 22:17:29 +0100

    Day 15 was spicy!

--- AdventOfCode2022/Day15.cs
+++ AdventOfCode2022/Day15.cs
@@ -27,111 +27,130 @@ public static class Day15 {
         var minY = pairs.Min(p => Math.Min(Math.Min(p.beacon.y, p.sensor.y), p.sensor.y - p.ManhattanDistance));
 
         minY = Math.Max(0, minY);
-        maxY = Math.Min(maxY, 4000000);
+        maxY = Math.Min(maxY, example ? 20 : 4000000);
         
         minX = Math.Max(0, minX);
-        maxX = Math.Min(maxX, 4000000);
+        maxX = Math.Min(maxX, example ? 20 : 4000000);
 
         if (example) {
-            var toConsider = pairs.First(p => p.sensor == new Point(8, 7));
-            for (int y = minY; y <= maxY; y++) {
-                var covered = CoveredByPairOnRow(toConsider, y);
-                Console.Out.Write(y);
-                Console.Out.Write(' ');
-                for (int x = minX; x <= maxX; x++) {
-                    var point = new Point(x, y);
-                    if (pairs.Any(p => p.sensor == point))
-                        Console.Out.Write('S');
-                    else if (pairs.Any(p => p.beacon == point))
-                        Console.Out.Write('B');
-                    else if (covered.Contains(x))
-                        Console.Out.Write('#');
-                    else
-                        Console.Out.Write('.');
-                }
-
-                Console.Out.WriteLine();
-            }
+            if (minY != 0 || maxY != 20 || minX != 0 || maxX != 20)
+                throw new();
+        }
+        else {
+            if (minY != 0 || maxY != 4000000 || minX != 0 || maxX != 4000000)
+                throw new();
         }
 
-        var rowToCheck = example ? 10 : 2000000;
-        var coveredOnRow = new HashSet<int>();
 
-        foreach (var pair in pairs)
-        foreach (var point in CoveredByPairOnRow(pair, rowToCheck))
-            coveredOnRow.Add(point);
+        var rowToCheck = example ? 10 : 2000000;
+
+        var ranges = new List<Range>();
+        foreach (var pair in pairs) {
+            var rangeCoveredOnRow = RangeCoveredByPairOnRow(pair, rowToCheck);
+            if (rangeCoveredOnRow.HasValue)
+                ranges.Add(rangeCoveredOnRow.Value);
+        }
+
+        CombineRanges(ranges);
 
         var beaconsOnRow = new HashSet<int>();
         foreach (var pair in pairs)
             if (pair.beacon.y == rowToCheck)
                 beaconsOnRow.Add(pair.beacon.x);
-
-        Console.Out.WriteLine($"Puzzle 1: {(coveredOnRow.Count - beaconsOnRow.Count)}");
-
-        Console.Out.WriteLine("How much for Puzzle 2, really?");
-        Console.Out.WriteLine($"x: {minX} - {maxX}, y: {minY} - {maxY}");
-        Console.Out.WriteLine((maxX - minX) * (maxY - minY));
-
-        var dumbArray = new byte[maxX - minX + 1, maxY - minY + 1];
-        var coveredByPair = new List<Point>();
-        for (var i = 0; i < pairs.Count; i++) {
-            var pair = pairs[i];
-            Console.Out.WriteLine($"Pair {i} out of {pairs.Count}");
-            FindCoveredByPair(pair, coveredByPair);
-            foreach (var point in coveredByPair) 
-                dumbArray[point.x - minX, point.y - minY] = 1;
-        }
-
-        Point result = default;
         
-        for (int x = minX; x <= maxX; x++)
-        for (int y = minY; y <= maxY; y++) {
+        Console.Out.WriteLine($"Puzzle 1: {(ranges.Sum(range => range.to - range.from + 1) - beaconsOnRow.Count)}");
+
+        // Okay, the way to do this is to find the _range_ covered on the row for each pair.
+        // Then, combine the ranges 
+        // Then, check if the number of ranges is less than 1
+        
+        for (int y = minY; y < maxY; y++) {
+            ranges.Clear();
+            foreach (var pair in pairs) {
+                var rangeCoveredOnRow = RangeCoveredByPairOnRow(pair, y);
+                if (rangeCoveredOnRow.HasValue) {
+                    var clampedRange = rangeCoveredOnRow.Value.Clamp(0, maxX);
+                    
+                    ranges.Add(clampedRange);
+                }
+            }
+            CombineRanges(ranges);
             
+            if ((y % 1000) == 0)
+                Console.Out.WriteLine($"y: {y}, ranges: [{string.Join(',', ranges)}]");
+            
+            if (ranges.Count == 2 || ranges[0].from != 0 || ranges[0].to != 4000000) {
+                Console.Out.WriteLine($"It's on row {y} with {ranges.Count} ranges: {string.Join(',', ranges)}");
+
+                Point distressBeaconOn;
+                if (ranges.Count == 2) {
+                    var minRange = ranges[0].from < ranges[1].from ? ranges[0] : ranges[1];
+                    distressBeaconOn = new Point(minRange.to + 1, y);
+                }
+                else if (ranges[0].from != 0) {
+                    if (ranges[0].from != 1)
+                        throw new();
+                    distressBeaconOn = new Point(0, y);
+                }
+                else if (ranges[0].to != 4000000) {
+                    if (ranges[0].to != 3999999)
+                        throw new();
+                    distressBeaconOn = new Point(4000000, y);
+                }
+                else
+                    throw new();
+
+                Console.Out.WriteLine("Distress beacon is on: " + distressBeaconOn);
+                Console.Out.WriteLine($"Puzzle 2: {(4000000 * distressBeaconOn.x) + distressBeaconOn.y}");
+                
+                break;
+            }
         }
     }
 
-    private static void FindCoveredByPair(SensorBeaconPair pair, List<Point> result) {
-        var pairDistance = pair.ManhattanDistance;
-        result.Clear();
-
-        var startRow = pair.sensor.y - pairDistance;
-        var endRow = pair.sensor.y + pairDistance;
-        
-        for (int row = startRow; row <= endRow; row++) {
-            var distanceToRow = Math.Abs(row - pair.sensor.y);
-
-            var halfWidth = pairDistance - distanceToRow;
-            if (halfWidth >= 0) {
-                var startX = pair.sensor.x - halfWidth;
-                var endX = pair.sensor.x + halfWidth;
-
-                for (int x = startX; x <= endX; x++) {
-                    result.Add(new Point(x, row));
+    private static void CombineRanges(List<Range> ranges) {
+        bool didCombine = true;
+        while (didCombine) {
+            didCombine = false;
+            
+            for (int i = 0; i < ranges.Count; i++)
+            for (int j = ranges.Count - 1; j > i; j--) {
+                var r1 = ranges[i];
+                var r2 = ranges[j];
+                if (Overlaps(r1, r2)) {
+                    ranges.RemoveAt(j);
+                    ranges[i] = new(Math.Min(r1.from, r2.from), Math.Max(r1.to, r2.to));
+                    didCombine = true;
                 }
             }
         }
     }
 
-    public static List<int> CoveredByPairOnRow(SensorBeaconPair pair, int row) {
-        var result = new List<int>();
+    private static bool Overlaps(Range r1, Range r2) {
+        if (r1.to < r2.from || r2.to < r1.from)
+            return false;
 
+        return true;
+    }
+
+    public static Range? RangeCoveredByPairOnRow(SensorBeaconPair pair, int row) {
         var pairDistance = pair.ManhattanDistance;
         var distanceToRow = Math.Abs(row - pair.sensor.y);
 
         var halfWidth = pairDistance - distanceToRow;
-        if (halfWidth >= 0) {
-            var startX = pair.sensor.x - halfWidth;
-            var endX = pair.sensor.x + halfWidth;
+        var startX = pair.sensor.x - halfWidth;
+        var endX = pair.sensor.x + halfWidth;
 
-            for (int x = startX; x <= endX; x++) {
-                result.Add(x);
-            }
-        }
-
-        return result;
+        return startX <= endX ? new Range(startX, endX) : null;
     }
 
     public record struct SensorBeaconPair(Point sensor, Point beacon) {
         public int ManhattanDistance => Math.Abs(sensor.x - beacon.x) + Math.Abs(sensor.y - beacon.y);
+    }
+
+    public record struct Range(int from, int to) {
+        public Range Clamp(int minFrom, int maxTo) {
+            return new Range(Math.Max(from, minFrom), Math.Min(to, maxTo));
+        }
     }
 }
