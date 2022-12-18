f2fe568c9dc30d6c4d65b7be78b994b2d8564305
Baste Nesse Buanes <baste@rain-games.com>
2022-12-18 15:40:53 +0100

    Day 17 part 1

--- AdventOfCode2022/Day17.cs
+++ AdventOfCode2022/Day17.cs
@@ -3,10 +3,11 @@ using static AdventOfCode2022.Day17.Space;
 namespace AdventOfCode2022;
 
 public static class Day17 {
-    private const int chamberWidth = 7;
+    private const int chuteWidth = 7;
 
     public static void Run(bool example) {
-        var data = File.ReadAllLines(example ? "day17Example" : "day17Input");
+        var data = File.ReadAllText(example ? "day17Example" : "day17Input");
+        var jets = data.Select(t => t == '>').ToArray(); // true: push right
 
         var chute = new List<Space[]>();
         chute.Add(LineOf(Rock));
@@ -14,18 +15,35 @@ public static class Day17 {
         var rockOrder = BuildRockOrder();
 
         var indexOfTopRocks = 0;
+        var jetIndex = 0;
         var nextRockToFall = 0;
 
-        for (int i = 0; i < 4; i++) {
+        var startTime = DateTime.Now;
+        for (int i = 0; i < 2022; i++) {
             var rock = rockOrder[nextRockToFall];
             nextRockToFall = (nextRockToFall + 1) % rockOrder.Count;
 
             var fallFrom = new Point(2, indexOfTopRocks + 4);
             
-            DrawChute(chute, rock, fallFrom);
-            MakeRockFall(chute, rock, fallFrom, out indexOfTopRocks);
-            DrawChute(chute);
+            // DrawChute(chute, rock, fallFrom);
+            MakeRockFall(chute, rock, fallFrom, jets, ref jetIndex, out var topOfFallenRock);
+            // DrawChute(chute);
+
+            if (topOfFallenRock > indexOfTopRocks)
+                indexOfTopRocks = topOfFallenRock;
         }
+        var endTime = DateTime.Now;
+        var duration = (endTime - startTime);
+        
+        DrawChute(chute);
+        Console.Out.WriteLine($"Puzzle 1: {indexOfTopRocks}");
+        Console.Out.WriteLine($"It took {duration.TotalMilliseconds} milliseconds!");
+
+        var durationOfAllRocks = (1000000000000 / 2022f) * duration;
+        Console.Out.WriteLine($"Puzzle 2 should take {durationOfAllRocks.TotalSeconds} seconds");
+        Console.Out.WriteLine(1000000000000);
+        Console.Out.WriteLine(1514285714288);
+        Console.Out.WriteLine(int.MaxValue);
     }
 
     private static void DrawChute(List<Space[]> chute, Space[,]? rock = null, Point rockAtPoint = default, bool waitForInput = true) {
@@ -80,15 +98,31 @@ public static class Day17 {
         }
     }
 
-    private static void MakeRockFall(List<Space[]> chute, Space[,] rock, Point currentRockPos, out int topOfRocks) {
+    private static void MakeRockFall(List<Space[]> chute, Space[,] rock, Point currentRockPos, bool[] jets, ref int jetIndex, out int topOfRocks) {
         MakeRoomForRockInChute(chute, rock, currentRockPos);
 
-        while (CanPlaceRockAt(chute, rock, currentRockPos)) 
-            currentRockPos.y--;
-        currentRockPos.y++;
+        while (true) {
+            var jetPushesRight = jets[jetIndex++];
+            if (jetIndex == jets.Length)
+                jetIndex = 0;
+
+            var push = jetPushesRight ? new Point(1, 0) : new Point(-1, 0);
+
+            if (CanPlaceRockAt(chute, rock, currentRockPos + push)) 
+                currentRockPos += push;
+
+            // DrawChute(chute, rock, currentRockPos);
+
+            if (CanPlaceRockAt(chute, rock, currentRockPos + new Point(0, -1))) {
+                currentRockPos.y--;
+                // DrawChute(chute, rock, currentRockPos);
+            }
+            else
+                break;
+        }
 
         InsertRock(chute, rock, currentRockPos);
-        topOfRocks = currentRockPos.y;
+        topOfRocks = currentRockPos.y + rock.GetLength(0) - 1;
     }
 
     private static void MakeRoomForRockInChute(List<Space[]> chute, Space[,] rock, Point currentRockPos) {
@@ -100,11 +134,16 @@ public static class Day17 {
     private static bool CanPlaceRockAt(List<Space[]> chute, Space[,] rock, Point currentRockPos) {
         var startInsertingAtY = currentRockPos.y;
         var startInsertingAtX = currentRockPos.x;
-
+        
         for (int y = 0; y < rock.GetLength(0); y++)
-        for (int x = 0; x < rock.GetLength(1); x++)
-            if (rock[y, x] == Rock && chute[y + startInsertingAtY][x + startInsertingAtX] == Rock)
+        for (int x = 0; x < rock.GetLength(1); x++) {
+            var insertingAtX = x + startInsertingAtX;
+            if (insertingAtX is < 0 or >= chuteWidth)
                 return false;
+            
+            if (rock[y, x] == Rock && chute[y + startInsertingAtY][insertingAtX] == Rock)
+                return false;
+        }
 
         return true;
     }
@@ -152,8 +191,8 @@ public static class Day17 {
 
 
     public static Space[] LineOf(Space space) {
-        var line = new Space[chamberWidth];
-        for (int i = 0; i < chamberWidth; i++)
+        var line = new Space[chuteWidth];
+        for (int i = 0; i < chuteWidth; i++)
             line[i] = space;
         return line;
     }
